49f3d3de86802f08eb06ce59fa75d7a9e9859ef4
Chloef33 <104874388+Chloef33@users.noreply.github.com>
2022-05-05 20:06:34 -0500

    Updated code to add columns

--- cleaning_data.R
+++ cleaning_data.R
@@ -12,3 +12,10 @@ df$SexUpdated <- gsub("(?i)Male|(?i)M", "male",df$SexUpdated)
 df$SexUpdated[df$SexUpdated == "female?"] <- "female"
 df$SexUpdated[df$SexUpdated =="male?"] <- "male"
 df$SexUpdated[is.na(df$SexUpdated)] <- "unknown"
+
+df_butterfly <- read_excel("data/CompletePierisData_2022-03-09.xlsx", sheet = 1)
+df_butterfly_clean <- df_butterfly %>%
+  dplyr::Country == df_butterfly$`dwc:country`
+dplyr::select("coreid", "SexUpdated", "LWingLength", "LWingWidth","LBlackPatchApex",
+              "LAnteriorSpotM3", "LPosteriorSpotCu2", "RWingLength", "RWingWidth",
+              "RBlackPatchApex","RAnteriorSpotM3", "RPosteriorSpotCu2","dwc:country")
