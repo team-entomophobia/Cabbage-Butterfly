bd246c39228afc0da6457fbe0fc7e8ea810d4f71
baotran19 <trandinhgiabao2001@gmail.com>
2022-05-12 20:06:57 -0500

    upload t-test wingspan by continent

--- t-test.R
+++ t-test.R
@@ -7,9 +7,16 @@ setwd("H:/Cabbage-Butterfly")
 df <- read.csv("data/clean_data.csv")
 
 df_europe <- df %>%
-  dplyr::filter(country == "United Kingdom" | country == "Republic of Ireland")
+  dplyr::filter(country == "United Kingdom" | country == "Republic of Ireland") %>%
+  dplyr::mutate(leftArea = LWingLength*LWingWidth, rightArea = RWingLength*RWingWidth)
+
 
 df_america <- df %>%
-  dplyr::filter(country == "United States" | country == "canada")
+  dplyr::filter(country == "United States" | country == "Canada") %>%
+  dplyr::mutate(leftArea = LWingLength*LWingWidth, rightArea = RWingLength*RWingWidth)
 
-t.test(df_europe$LWingLength,df_america$LWingLength, var.equal = FALSE)
+
+#t.test(df_europe$LWingLength, df_america$LWingLength, alternative = "less", var.equal = FALSE)
+
+t.test(df_europe$leftArea, df_america$leftArea, alternative = "less", var.equal = FALSE)
+t.test(df_europe$rightArea, df_america$rightArea, alternative = "less", var.equal = FALSE)
