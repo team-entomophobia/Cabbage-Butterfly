17c4230542321a780786f5a15b3256226381d21b
baotran19 <trandinhgiabao2001@gmail.com>
2022-05-12 19:06:08 -0500

    upload charts

---
+++ decade.R
@@ -0,0 +1,22 @@
+library(tidyverse)
+library(dplyr)
+library(readxl)
+library(lubridate)
+
+#set up working directory and the raw file
+rm(list = ls())
+setwd("/Users/baodinhgiatran/Desktop/DATA 332/cabbage_butterfly-main")
+df <- read.csv("testing.csv") 
+
+df1 <- df %>%
+  dplyr::select("coreid", "year")
+df1$year <-substring(df1$year, 1,3)
+df1$year <- paste(df1$year, "0", sep = "")
+
+df2 <- df1 %>%
+  count(year)
+
+ggplot(data = df2, aes(x=year, y = n, group = 1)) +
+  geom_line(color = "blue") +
+  ggtitle("Number of butterflies recorded by decade")+
+  labs(x = "decades", y = "number of butterflies")+ geom_point()
---
+++ min_max.R
@@ -0,0 +1,22 @@
+library(tidyverse)
+library(dplyr)
+library(readxl)
+library(lubridate)
+library(reshape)
+
+#set up working directory and the raw file
+rm(list = ls())
+setwd("/Users/baodinhgiatran/Desktop/DATA 332/cabbage_butterfly-main")
+df <- read.csv("testing.csv") 
+
+df1 <- df %>%
+  group_by(sex) %>%
+  summarise(min.LWingLength = min(LWingLength),
+            mean.LWingLength = mean(LWingLength),
+            median.LWingLength = median(LWingLength),
+            max.LWingLength = max(LWingLength))
+
+df2 <- pivot_longer(df1, cols = 2:5, names_to = "functions", values_to = "value")
+
+ggplot(df2, aes(fill = functions, x = sex, y = value)) +
+  geom_bar(position = "dodge", stat = "identity")
