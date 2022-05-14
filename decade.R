727db3dddc8906041b28a4f6fcbd448ded8a276a
baotran19 <trandinhgiabao2001@gmail.com>
2022-05-14 14:14:58 -0500

    line_chart_by_decade_DONE

--- decade.R
+++ decade.R
@@ -17,7 +17,7 @@ df2 <- df1 %>%
   count(year)
 
 ggplot(data = df2, aes(x=year, y = n, group = 1)) +
-  geom_line(color = "#FFCC33") +
+  geom_line(color = "#FF6600") +
   ggtitle("Number of Butterflies recorded by Decades")+
-  labs(x = "Decades", y = "Number of Butterflies")+ geom_point(color = "#003366")+
+  labs(x = "Decades", y = "Number of Butterflies")+ geom_point(color = "#003399")+
   theme_minimal()
