01f674fcdfb7cede8c7fca092537cb4db1fe27c0
baotran19 <trandinhgiabao2001@gmail.com>
2022-05-13 20:09:03 -0500

    update theme and color

--- decade.R
+++ decade.R
@@ -11,12 +11,13 @@ df <- read.csv("data/clean_data.csv")
 df1 <- df %>%
   dplyr::select("coreid", "year")
 df1$year <-substring(df1$year, 1,3)
-df1$year <- paste(df1$year, "0", sep = "")
+df1$year <- paste(df1$year, "0s", sep = "")
 
 df2 <- df1 %>%
   count(year)
 
 ggplot(data = df2, aes(x=year, y = n, group = 1)) +
-  geom_line(color = "blue") +
-  ggtitle("Number of butterflies recorded by decade")+
-  labs(x = "decades", y = "number of butterflies")+ geom_point()
+  geom_line(color = "#FFCC33") +
+  ggtitle("Number of Butterflies recorded by Decades")+
+  labs(x = "Decades", y = "Number of Butterflies")+ geom_point(color = "#003366")+
+  theme_minimal()
