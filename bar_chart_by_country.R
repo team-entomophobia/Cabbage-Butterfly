53949f664fc8a26ada14b03f90d51025f7f5409f
baotran19 <trandinhgiabao2001@gmail.com>
2022-05-13 19:56:05 -0500

    clean the chart

--- bar_chart_by_country.R
+++ bar_chart_by_country.R
@@ -16,5 +16,7 @@ df1 <- df %>%
 ggplot(df1, aes(fill = sex, y = n, x = country)) +
   geom_bar(position = "stack", stat = "identity") +
   ggtitle("Number of Butterflies recorded by Sex and Country") +
-  labs(x = "Country", y = "Number of Butterflies")
-
+  labs(x = "Country", y = "Number of Butterflies") +
+  scale_fill_manual(values = c("#FFCC33", "#003366"), 
+                    name = "Sex", labels = c("Female", "Male"))+
+  theme_minimal()
